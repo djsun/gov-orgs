85b3c8600fe8ef337c2798486e272bcb394feea5
David James <djames@sunlightfoundation.com>
2009-11-19 18:10:28 -0500

    improving name standardization

--- migrations/0005-standardize-names.rb
+++ migrations/0005-standardize-names.rb
@@ -13,49 +13,74 @@ class NameStandardizer
     Utility.modify_each_org(MASTER_FILE, TEMP_FILE) do |org|
       data = org['versions'][0]['data'].deep_clone
       names = data['names']
-      s_names = standardize_names(names)
-      data['names'] = (s_names + names).uniq
-      new_version = YAML::Omap[
-        'data', data,
-        'time', Utility.time_format(Time.now),
-        'who',  File.basename(__FILE__),
-      ]
-      org['versions'].insert(0, new_version)
+      simplified_names = simplify_names(names)
+      standardized_names = standardize_names(simplified_names)
+      new_names = (standardized_names + simplified_names).uniq
+      if new_names != names
+        data['names'] = new_names
+        new_version = YAML::Omap[
+          'data', data,
+          'time', Utility.time_format(Time.now),
+          'who',  File.basename(__FILE__),
+        ]
+        org['versions'].insert(0, new_version)
+      end
     end
   end
   
   protected
   
+  def simplify_names(names)
+    names.map { |s| simplify(s) }.uniq
+  end
+  
   def standardize_names(names)
-    names.map { |name| standardize(name) }.uniq
+    names.map { |s| standardize(s) }.uniq
   end
 
   def simplify(name)
     name.
-      gsub(/generally, no additional specification available/i, '').
-      gsub(/- except .*/i, '').
-      gsub(/, except .*/i, '').
-      gsub(/\(except .*/i, '').
-      gsub(/  /, ' ').strip.
-      gsub(/,$/, '').strip
+    gsub(/generally, no additional specification available/i, '').
+    gsub(/- except .*/i, '').
+    gsub(/, except .*/i, '').
+    gsub(/\(except .*/i, '').
+    gsub(/\//, ' / ').
+    gsub(/  /, ' ').
+    gsub(/,$/, '').
+    strip
   end
   
   STANDARDIZATION_PATTERNS = [
-    [/\bAdmin\.\b/     , 'Administration'    ],
-    [/\bAssist\.\b/    , 'Assistant'         ],
-    [/\bImmed\.\b/     , 'Immediate'         ],
-    [/\bIntergov\.\b/  , 'Intergovernmental' ],
-    [/\bInternat'l\b/  , 'International'     ],
-    [/\bNat'l\b/       , 'National'          ],
-    [/\bSec'y\b/       , 'Secretary'         ],
-    [/\bSec\.\b/       , 'Secretary'         ],
-    [/&/               , ' and '             ],
-    [/\//              , ' / '               ],
-    [/  /              , ' '                 ],
+    [/Admin\./          , 'Administration'          ],
+    [/Assist\./         , 'Assistant'               ],
+    [/Cntr/             , 'Center'                  ],
+    [/Commun\./         , 'Communications'          ],
+    [/Comm\.-In-Chief/  , 'Commander-In-Chief'      ],
+    [/Dep\. Comm\./     , 'Deputy Commissioner'     ],
+    [/Develop\./        , 'Development'             ],
+    [/Econ\./           , 'Economic'                ],
+    [/Ed\./             , 'Education'               ],
+    [/Enviro\./         , 'Environmental'           ],
+    [/Equip\./          , 'Equipment'               ],
+    [/Immed\./          , 'Immediate'               ],
+    [/Info\./           , 'Information'             ],
+    [/Intergov\./       , 'Intergovernmental'       ],
+    [/Internat'l/       , 'International'           ],
+    [/Mgmt\./           , 'Management'              ],
+    [/Nat'l/            , 'National'                ],
+    [/Preserv\./        , 'Preservation'            ],
+    [/Rehab\. Services/ , 'Rehabilitation Services' ],
+    [/Scholar\./        , 'Scholarship'             ],
+    [/Sec'y/            , 'Secretary'               ],
+    [/Sec\./            , 'Secretary'               ],
+    [/Spec\. Ed/        , 'Special Ed'              ],
+    [/Tank-Auto\./      , 'Tank-Automotive'         ],
+    [/&/                , ' and '                   ],
+    [/  /               , ' '                       ],
   ]
 
   def standardize(name)
-    name = simplify(name.dup)
+    name = name.dup
     STANDARDIZATION_PATTERNS.each do |x|
       name.gsub!(x[0], x[1])
     end
